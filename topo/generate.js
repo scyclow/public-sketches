5bd4ac0d839b66d9d5f5065449a8b8434064166a
Steve Pikelny <spike716@gmail.com>
2021-10-27 10:48:11 -0400

    lazy load

--- topo/generate.js
+++ topo/generate.js
@@ -3,6 +3,6 @@ const fs = require('fs')
 const images = fs.readdirSync('./images')
 
 const tags = images.filter(src => src.includes('.jpg')).map((src, i) => `
-  <div class="img"><img src="./images/${src}" alt="topo-sketch ${src}">${i} -- ${new Date(Number(src.replace('topo-', '').replace('.jpg', '')))}</div>`)
+  <div class="img"><img src="./images/${src}" alt="topo-sketch ${i}" loading="lazy">${i} -- ${new Date(Number(src.replace('topo-', '').replace('.jpg', '')))}</div>`)
 
 fs.writeFileSync('./images.html', tags.join(`\n`))
