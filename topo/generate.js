fd3b7b965e7eb79e67f3db4ff9eac9780dce2b6f
Steve Pikelny <spike716@gmail.com>
2021-10-27 10:44:16 -0400

    init

---
+++ topo/generate.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+
+const images = fs.readdirSync('./images')
+
+const tags = images.filter(src => src.includes('.jpg')).map((src, i) => `
+  <div class="img"><img src="./images/${src}" alt="topo-sketch ${src}">${i} -- ${new Date(Number(src.replace('topo-', '').replace('.jpg', '')))}</div>`)
+
+fs.writeFileSync('./images.html', tags.join(`\n`))
